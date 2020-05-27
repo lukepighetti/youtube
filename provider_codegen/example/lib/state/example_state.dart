a1fa30267fdbcefee3d6e4aa3b3b0131164c93b0
lukepighetti <lukepighetti@gmail.com>
2020-05-27 13:36:10 -0400

    get it working

---
+++ provider_codegen/example/lib/state/example_state.dart
@@ -0,0 +1,12 @@
+import 'package:state_annotation/state_annotation.dart';
+
+part 'example_state.state.dart';
+
+@StateClass()
+class ExampleState extends _$ExampleStateChangeNotifier {
+  @StateField()
+  String get foo => _foo;
+
+  @StateField()
+  bool get isAwesome => _isAwesome;
+}
--- provider_codegen/state_codegen/lib/state_codegen.dart
+++ provider_codegen/state_codegen/lib/state_codegen.dart
@@ -1,7 +1,48 @@
 library state_codegen;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/dart/element/type.dart';
+import 'package:analyzer/dart/element/visitor.dart';
+import 'package:build/build.dart';
+import 'package:source_gen/source_gen.dart';
+import 'package:state_annotation/state_annotation.dart';
+
+class StateGenerator extends GeneratorForAnnotation<StateClass> {
+  @override
+  generateForAnnotatedElement(
+      Element element, ConstantReader annotation, BuildStep buildStep) {
+    final visitor = GetterFieldVisitor();
+    element.visitChildren(visitor);
+
+    return [
+      "class _\$${visitor.className}ChangeNotifier {",
+      for (var getter in visitor.getters.entries)
+        "${getter.value.returnType} _${getter.key};",
+      "}"
+    ];
+  }
+}
+
+/// Dispatches build commands to [StateGenerator]
+Builder stateBuilder(BuilderOptions options) =>
+    PartBuilder([StateGenerator()], ".state.dart");
+
+/// Returns field getters on an element
+class GetterFieldVisitor extends SimpleElementVisitor {
+  DartType className;
+  Map<String, PropertyAccessorElement> getters = {};
+
+  @override
+  visitConstructorElement(ConstructorElement element) {
+    className = element.type.returnType;
+  }
+
+  @override
+  visitFieldElement(FieldElement element) {
+    final getter = element.getter;
+
+    if (getter != null) {
+      getters[element.name] = getter;
+    }
+  }
 }
